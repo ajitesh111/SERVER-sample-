e7f0f952536dfaafcc3a2a4c2dd7e7f4de989f8f
ajitesh <ajiteshjaiswal11@gmail.com>
2021-11-25 14:11:55 +0530

    added data validation and frontend changes

--- index.js
+++ index.js
@@ -100,20 +100,14 @@ app.put('/api/persons/:id', (request,response,next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request,response) => {
+app.post('/api/persons', (request,response,next) => {
   const body = request.body
 
-  if(!body.name || !body.number)
-  {
-    return response.status(400).json({
-      error: 'content-missing'
-    })
-  }
-
-  // if(persons.find(person => person.name === body.name))
+  //conditions are defined in the schema definition in person.js
+  // if(!body.name || !body.number)
   // {
-  //   return response.status(409).json({
-  //     error: `a record with name ${body.name} already exists in the phonebook`
+  //   return response.status(400).json({
+  //     error: 'content-missing'
   //   })
   // }
 
@@ -127,9 +121,12 @@ app.post('/api/persons', (request,response) => {
   //   number: body.number
   // }
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
+  person.save()
+    .then(savedPerson => { //schema constraints are checked here (if name already added)
+      response.json(savedPerson)
   })
+    .catch(error => next(error))
+
   // persons = persons.concat(newPerson)
   // response.json(newPerson)
 })
@@ -140,6 +137,8 @@ const errorHandler = (error, request, response, next) => {
 
   if(error.name === 'CastError')
     return response.status(400).send({error: 'malformatted id'})
+  else if(error.name === 'ValidationError')
+    return response.status(403).send(error.message)
 
   next(error) //forwards unhandled errors to the default error handler of express
 }
--- models/person.js
+++ models/person.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 
+const uniqueValidator = require('mongoose-unique-validator')
+
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
@@ -13,8 +15,27 @@ mongoose.connect(url)
     })
 
 const phonebookSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true,
+        unique: true    //uniqueValidator will work on this attribute
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        required: true
+    }
+})
+//checks if the fields with unique: true is unique
+phonebookSchema.plugin(uniqueValidator)
+
+//by Default validator does not run on updates in mongoose
+//pre is a middleware that changes how 'findOneAndUpdate' functions
+//findByIdAndUpdate calls findOneAndUpdate
+phonebookSchema.pre('findOneAndUpdate', function(next) {
+    this.options.runValidators = true
+    next()
 })
 
 phonebookSchema.set('toJSON', { //all instances of models produced with this schema will be modified, response.json callbacks to this function (probably)
